381bb0b5c57eab3d1a996d97bfc28bc514192faa
moba0222 <mohammad.babu.miya@netcracker.com>
2024-08-02 12:40:34 +0530

    02-08-2024 - tech learning starting-05

---
+++ CurrentProfile_QnS/JS/Concepts/a_Freshers/2. What are JavaScript Data Types/1.Number Datatype/4.Practice2_Number_Propeties.js
@@ -0,0 +1,12 @@
+// Number object is the default JS objects 
+// Number object properties availabe in JS
+let num0 = Number.MAX_VALUE; //The maximum numeric value representable in JavaScript.
+let num1 = Number.MIN_VALUE; // The smallest positive numeric value representable in JavaScript.
+let num2 = Number.POSITIVE_INFINITY; //Represents positive infinity.
+let num3 = Number.NEGATIVE_INFINITY; //Represents negative infinity.
+let num4 = Number.NaN; //Represents a value that is "Not-a-Number".||  case sensitive NAN will be undefined
+console.log(num0);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
---
+++ CurrentProfile_QnS/TS/Concepts/1. The data types available in TypeScript/1.Number Datatypes/4.Practice2_Number_Properties.ts
@@ -0,0 +1,12 @@
+let numb0: number = Number.MAX_VALUE;
+let numb1: number = Number.MIN_VALUE;
+let numb2: number = Number.POSITIVE_INFINITY;
+let numb3: number = Number.NEGATIVE_INFINITY;
+let numb4: number = Number.NaN;
+
+console.log(numb0);
+console.log(numb1);
+console.log(numb2);
+console.log(numb3);
+console.log(numb4);
+
